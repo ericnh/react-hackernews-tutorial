3d3870d0c0a924a24a227516e827db8c6f2fa54e
eric <halstead.eric@gmail.com>
2017-12-02 15:26:00 -0800

    Clean up table view and fix blank target bug

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,19 +5,19 @@ const Table = ({ list, onDismiss }) =>
   <div className="table">
     { list.map( item =>
         <div key={ item.objectID } className="table-row">
-          <span style={{ width: '40%' }}>
-            <a href={ item.url }>{ item.title }</a>
+          <span style={{ width: '40%' }} title ="View Page">
+            <a href={ item.url } target="_blank" rel="noopener">{ item.title }</a>
           </span>
-          <span style={{ width: '30%' }}>
+          <span style={{ width: '30%' }} title="Author">
             { item.author }
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={{ width: '10%' }} title="Comments">
             { item.num_comments }
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={{ width: '10%' }} title="Points">
             { item.points }
           </span>
-          <span style={{ width: '10%' }}>
+          <span style={{ width: '10%' }} title="Dismiss Item">
             <Button
               className="button-inline"
               onClick={() => onDismiss(item.objectID)}
